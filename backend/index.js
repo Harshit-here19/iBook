8b47dc8bb0380125c0e1fabb21f13f473969cb03
Harshit-here19 <Harshitsikarwar232@gmail.com>
2024-08-30 22:36:16 +0530

    Allowing CORS

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 const app = express();
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: 'https://ibook-cloud.onrender.com/',
     credentials: true,
     allowedHeaders: ['Content-Type', 'auth-token']
 }
